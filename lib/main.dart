6fc6db4ca71d63703d9466448c160fe77ea9f4c8
Abdoulaye issa <Issaabdoulaye>
2025-10-30 22:40:05 +0100

    ajout

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
